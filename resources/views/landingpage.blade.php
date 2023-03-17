de0dddb4b8bddb78d4afd119905d4f1b026eaf0b
sebas320122 <sebas320122@gmail.com>
2023-03-17 10:17:46 -0700

    actividad9

---
+++ resources/views/landingpage.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('LandingPage') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('Favor de ingresar una cuenta registrada o crear una nueva') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
